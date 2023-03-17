05cc12a7cefda911f6de9559cf3273af5039a882
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-17 11:27:23 +0000

    Exercise 01, reads input

---
+++ ChangeString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a sentence please");
+            var sentence = Console.WriteLine("");
+
+            Console.WriteLine("Now write a letter");
+            var letter = Console.WriteLine("");
+
+            Console.WriteLine(letter);
+        }
+    }
+}
