b40f53d6c7869a5114317835a4d11e25503963f7
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-17 13:40:38 +0000

    Exercise 02, complete, needs bug fix

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -21,9 +21,33 @@ namespace ArraySums
                     Console.Write("Please input a number for the position (" + (idxV + 1) + " , " + (idxH + 1) + "): ");
                     arraybi[idxV, idxH] = int.Parse(Console.ReadLine()!);
                 }
+        }
+
+        }
+
+            var sumCols = new int[arraybi.GetLength(0)];
+            for (int idxV = 0; idxV < arraybi.GetLength(0); idxV++)
+            {
+                for (int idxH = 0; idxH < arraybi.GetLength(1); idxH++)
+                {
+                    sumCols[idxV] += arraybi[idxV, idxH];
+                }
+            }
+
+            var sumLines = new int[arraybi.GetLength(1)];
+            for (int idxV = 0; idxV < arraybi.GetLength(0); idxV++)
+            {
+                for (int idxH = 0; idxH < arraybi.GetLength(1); idxH++)
+                {
+                    sumLines[idxH] += arraybi[idxV, idxH];
+                }
+            }
+
+            Console.WriteLine("[{0}]", string.Join(", ", sumCols));
+            Console.WriteLine("[{0}]", string.Join(", ", sumLines));
 
 
-
+            Console.ReadKey(intercept: true);
         }
     }
 }
