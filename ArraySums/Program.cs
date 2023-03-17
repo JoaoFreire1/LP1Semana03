a06b0ec52adee44add7edeeb12411f662dde898a
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-17 13:22:53 +0000

    Exercise 02, Reads Array Position

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -9,8 +9,20 @@ namespace ArraySums
             Console.Write("Please input how big you want it Vertically: ");
             var vertical = Console.ReadLine()!;
 
-             Console.Write("Please input how big you want it Horizontally: ");
+            Console.Write("Please input how big you want it Horizontally: ");
             var horizontal = Console.ReadLine()!;
+
+            var arraybi = new int[int.Parse(vertical), int.Parse(horizontal)];
+
+            for (int idxV = 0; idxV < arraybi.GetLength(0); idxV++)
+            {
+                for (int idxH = 0; idxH < arraybi.GetLength(1); idxH++)
+                {
+                    Console.Write("Please input a number for the position (" + (idxV + 1) + " , " + (idxH + 1) + "): ");
+                    arraybi[idxV, idxH] = int.Parse(Console.ReadLine()!);
+                }
+
+
 
         }
     }
