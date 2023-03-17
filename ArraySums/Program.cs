eeb660087e5da39ecf9605f43423323b7efb4bf7
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-17 13:19:29 +0000

    Exercise 02, Reads Input

---
+++ ArraySums/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArraySums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input how big you want it Vertically: ");
+            var vertical = Console.ReadLine()!;
+
+             Console.Write("Please input how big you want it Horizontally: ");
+            var horizontal = Console.ReadLine()!;
+
+        }
+    }
+}
