988220d0b524c0b8bd71ef0a4b89f7faf6b4d401
JoaoFreire1 <joao.freire.art@gmail.com>
2025-03-06 16:31:10 +0000

    Read input

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -4,9 +4,12 @@ namespace RightSize
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main(strings args)
         {
-            Console.WriteLine("Hello LP1!");
+            foreach (string s in args)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
