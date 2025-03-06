ed92ed092ddf736f39fcdc5c2d737fce14362ec9
JoaoFreire1 <joao.freire.art@gmail.com>
2025-03-06 17:36:00 +0000

    Exercise 03

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -6,7 +6,22 @@ namespace ArrayMul
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(args[2]);
+            float valor1 = float.Parse(args[0]);
+            float valor2 = float.Parse(args[1]);
+            float valor3 = float.Parse(args[2]);
+            float valor4 = float.Parse(args[3]);
+            float valor5 = float.Parse(args[4]);
+            float valor6 = float.Parse(args[5]);
+
+            float result1 = valor1 * valor5;
+            float result2 = valor2 * valor6;
+            float resultf1 = result1 + result2;
+            float result3 = valor3 * valor5;
+            float result4 = valor4 * valor6;
+            float resultf2 = result3 + result4;
+
+            Console.WriteLine($"|  {resultf1}  |");
+            Console.WriteLine($"|  {resultf2}  |");
         }
     }
 }
