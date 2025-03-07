924fb764d01bf9abda52a41b27a15d3b86c11323
JoaoFreire1 <joao.freire.art@gmail.com>
2025-03-07 08:56:40 +0000

    Exercise 04

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -1,35 +1,103 @@
 ﻿using System;
-using System.Security.Permissions;
-using HeroPerks;
+using System.Text;
 
 namespace HeroPerk
 {
-    public class Program
+    /// <summary>
+    /// This is an example of summary
+    /// </summary>
+    class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            for (string s in args)
+            Perks perks = Perks.None;
+
+            string leitura = Console.ReadLine();
+            foreach (var item in leitura)
             {
-                switch (char r in string s)
+                switch (item)
                 {
-                    case w:
-                        myRoles ^=Perks.WarpShift;
-                        break;
-                
-                    case a:
-                        myRoles ^=Perks.AutoHeal;
-                        break;
-                
-                    case s:
-                        myRoles ^=Perks.Stealth;
-                        break;
-                
-                    case d:
-                        myRoles ^=Perks.DoubleJump;
+                    case 'a':
+                        if ((perks & Perks.AutoHeal) == Perks.AutoHeal)
+                        {
+                            perks &= ~Perks.AutoHeal;
+                        }
+                        else
+                        {
+                            perks ^= Perks.AutoHeal;
+                        }
                         break;
 
+                    case 's':
+                        if ((perks & Perks.Stealth) == Perks.Stealth)
+                        {
+                            perks &= ~Perks.Stealth;
+                        }
+                        else
+                        {
+                            perks ^= Perks.Stealth;
+                        }
+                        break;
+
+                    case 'w':
+                        if ((perks & Perks.WarpShift) == Perks.WarpShift)
+                        {
+                            perks &= ~Perks.WarpShift;
+                        }
+                        else
+                        {
+                            perks ^= Perks.WarpShift;
+                        }
+                        break;
+
+                    case 'd':
+                        if ((perks & Perks.DoubleJump) == Perks.DoubleJump)
+                        {
+                            perks &= ~Perks.DoubleJump;
+                        }
+                        else
+                        {
+                            perks ^= Perks.DoubleJump;
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("!Unknown perk!");
+                        return;
                 }
+
             }
+
+            if(perks != Perks.None)
+            {
+                Console.WriteLine(perks);
+            }
+            else
+            {
+                Console.WriteLine("!No perks at all!");
+            }
+
+            if (((perks & Perks.WarpShift) == Perks.WarpShift) && ((perks & Perks.DoubleJump) == Perks.DoubleJump))
+            {
+                Console.WriteLine("!Silent jumper!");
+            }
+
+            if ((perks & Perks.AutoHeal) != Perks.AutoHeal)
+            {
+                Console.WriteLine("!Not gonna make it!");
+            }
+            
+
         }
+    }
+
+    [Flags]
+    enum Perks
+    {
+        None = 0,          // No perks
+        WarpShift = 1,     // 0001
+        Stealth = 2,       // 0010
+        AutoHeal = 4,      // 0100
+        DoubleJump = 8     // 1000
     }
 }
